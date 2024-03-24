4b208701907e6e1815d3f3f0f5bd0299b197ba30
Kevin Huang <41244296+kh9sd@users.noreply.github.com>
2024-03-24 17:28:44 -0400

    working net????

--- warmup/model.py
+++ warmup/model.py
@@ -7,13 +7,38 @@ class SimpleModel(torch.nn.Module):
         self.label_num = label_num
         
         ## Modify the code below ##
-
+        """
+        Suggested Architecture: 
+        Conv2d(32, kernel=3, stride=1, padding=1) → 
+        ReLU → 
+        Pool(kernel=2, stride=2) → 
+        Conv2d(64, kernel=3, stride=1, padding=1) → 
+        ReLU → 
+        Pool(kernel=2, stride=2) → 
+        Flatten → 
+        FC →
+        Softmax
+        """
         self.layers = torch.nn.Sequential(
+            torch.nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2),
+
+            torch.nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2),
+
             torch.nn.Flatten(),
-            torch.nn.Linear(img_size[0]*img_size[1], label_num),
+            torch.nn.Linear(img_size[0]*img_size[1]*4, label_num),
             torch.nn.Softmax(dim=1)
         )
 
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.layers(x)
+        # for layer in self.layers:
+        #     print(layer)
+        #     x = layer(x)
+
+        #     print(x.size())
+        # return x
